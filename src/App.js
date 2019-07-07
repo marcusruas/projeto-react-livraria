31aa2d373ef6da4e6f323425fa212e7241b7a754
marcusruas <marcus.ruas@hotmail.com>
2019-07-07 19:54:55 -0300

    Definido layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './ui/css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="cabecalho">
       </header>
+      <nav className="App-menu">
+      </nav>
+      <section className="conteudo">
+      </section>
+      <footer className="rodape">
+      </footer>
     </div>
   );
 }
